e6f540148654d7777b4ebd7318d9d80f429758aa
Hassan Mehmood <mehassanhmood@gmail.com>
2024-10-22 17:27:45 -0400

    additional imports added

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup, Circle } from 'react-leaflet';
+import L from 'leaflet';
 
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 
 const MapComponent = (props) => {
