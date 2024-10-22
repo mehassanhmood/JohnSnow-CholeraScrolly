e4aaedf22ee797eabefda600621628d71e8161d2
Hassan Mehmood <mehassanhmood@gmail.com>
2024-10-22 03:18:48 -0400

    gh-page debugging3

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
 import { useState } from 'react'
-import { Box, Heading, VStack, Container } from '@chakra-ui/react';
-
-import ScrollyTelling from './components/ScrollyTelling';
-import StickyThing from './components/StickyThing';
 import SideStickyThing from './components/SideStickyThing';
 
 function App() {
