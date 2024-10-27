18a2c52dcd46aa9010e864006c8170f35997df27
Hassan Mehmood <mehassanhmood@gmail.com>
2024-10-27 01:31:19 -0400

    finalized

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,11 @@ import SideStickyThing from './components/SideStickyThing';
 
 function App() {
 
-  const { scrollYProgress } = useScroll();
+
 
   return (
     <>  
       <Box
-        as={motion.div}
-        position="fixed"
-        top={0}
-        left="50%"
-        width="10px"
-        height="100vh"
-        bg="lime.100"
-        style={{ scaleY: scrollYProgress }}
-        transformOrigin="top"
       />
        <div className="relative">
         <SideStickyThing />
